96eafa77eb01a5654bcbbb2a212680466b193523
Joel Kraft <joelkraft3@yahoo.com>
2017-01-12 15:07:36 -0800

    init

---
+++ multipler.js
@@ -0,0 +1,15 @@
+module.exports = (factor) => {
+  return (req, res, next) => {
+    if (req.body.number === undefined) {
+      return next();
+    }
+    const num = parseFloat(req.body.number, 10);
+    if (isNaN(num)) {
+      const err = new Error('Input was not a number');
+      next(err);
+    }
+    const result = num * factor;
+    res.doubled = result;
+    next();
+  }
+}
